facb0874c741557e2bbca8cf129d72b17c733db9
Gene McCulley <mcculley@stackframe.com>
2011-11-05 13:37:28 -0400

    Initial import.

---
+++ src/com/stackframe/sql/SQLUtilities.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2011 StackFrame, LLC
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.stackframe.sql;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility functions for use with the java.sql package.
+ *
+ * @author Gene McCulley
+ */
+public class SQLUtilities {
+
+    private SQLUtilities() {
+        // Inhibit construction as this class has only static functions.
+    }
+
+    /**
+     * Given a PreparedStatement, execute it and load all of the values into a List of Map objects, keyed by column name.
+     *
+     * @param statement the PreparedStatement to execute
+     * @return a List of Map objects where keys are column names and values are the values retrieved from the SQL query
+     * @throws SQLException if a SQLException was thrown when executing the query
+     */
+    public static List<Map<String, Object>> load(PreparedStatement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery();
+        try {
+            List<Map<String, Object>> entries = new ArrayList<Map<String, Object>>();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            while (rs.next()) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                for (int i = 1; i < columnCount + 1; i++) {
+                    String name = rsmd.getColumnName(i);
+                    Object value = rs.getObject(i);
+                    map.put(name, value);
+                }
+
+                entries.add(map);
+            }
+
+            return entries;
+        } finally {
+            rs.close();
+        }
+    }
+}
